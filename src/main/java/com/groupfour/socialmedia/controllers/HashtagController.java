9e534c761a04280bc7bb6f54f1b963fa1ac96051
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-02-26 17:37:29 -0800

    create Hashtag controller, Hashtag service, and Hashtag service implementation class

---
+++ src/main/java/com/groupfour/socialmedia/controllers/HashtagController.java
@@ -0,0 +1,27 @@
+package com.groupfour.socialmedia.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.groupfour.socialmedia.dtos.HashtagResponseDto;
+import com.groupfour.socialmedia.services.HashtagService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("hashtag")
+public class HashtagController {
+	
+	private final HashtagService hashtagService;
+
+	public List<HashtagResponseDto> getAllHashtags() {
+		return hashtagService.getAllHashtags();
+	}
+	
+	
+
+	
+}
